efb0c6fd4955db085c80b5b658fec5026e2461d4
인경수 <yinks94@gmail.com>
2024-10-01 16:25:44 +0900

    Create Webpack Project

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+
+const path = require('path')
+const HtmlPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+// export
+module.exports={
+    // parcel index.html
+    // 파일을 읽어들이기 시작하는 진입점 설정
+    entry: './js/main.js',
+
+    // 결과물(번들)을 변환하는 설정
+    output:{
+        // path: path.resolve(__dirname, 'dist'),
+        // filename: 'main.js',
+        clean: true
+    },
+
+    module:{
+        rules: [
+            {
+                test: /\.s?css$/,
+                use:[
+                    'style-loader',
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.js$/,
+                use:[
+                    'babel-loader'
+                ]
+            }
+        ]
+    },
+
+    // 번들링 후 결과물의 처리 방식 등 다양한 플러인 처리
+    plugins: [
+        new HtmlPlugin({
+                template: './index.html'
+        }),
+        new CopyPlugin({
+            patterns: [
+                {from: 'static'}
+            ]
+        })
+    ],
+    devServer: {
+        host: 'localhost'
+    }
+}
